899930b4614a7d9c4f79c8535ccf920d288ef1b6
eltigerchino <chewteeming01@gmail.com>
2024-12-19 10:56:17 +0800

    Initial commit

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page as pageState } from '$app/state'
+  import { page as pageStore } from '$app/stores'
+
+  console.log('state', pageState.url.host, pageState.params);
+  console.log('store', $pageStore.url.host, $pageStore.params);
+</script>
